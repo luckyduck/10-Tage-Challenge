1f16e153b4924fe5f71715812e1a0c77ff9ef974
Jan Brinkmann <lucky@the-luckyduck.de>
2016-10-24 10:37:18 +0200

    Video #2 - Variablen

---
+++ 2-Variablen/Konverter/src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * (c) codingtutor.de
+ * --
+ * Jan Brinkmann
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        // initialisierung
+        int alter = 34;
+
+        // deklaration
+        int gewicht;
+        // ... (Werte einlesen)
+        gewicht = 34;
+
+
+        System.out.println("Gewicht: " + gewicht);
+    }
+}
