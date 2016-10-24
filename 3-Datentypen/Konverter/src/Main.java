4b646f4c58e031b5833729982120450fb57c8008
Jan Brinkmann <lucky@the-luckyduck.de>
2016-10-24 10:50:47 +0200

    Video #3 - Datentypen

---
+++ 3-Datentypen/Konverter/src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * (c) codingtutor.de
+ * --
+ * Jan Brinkmann
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Variablen */
+
+        // initialisierung
+        int alter = 34;
+
+        // deklaration
+        int gewicht;
+        // ... (Werte einlesen)
+        gewicht = 34;
+
+        System.out.println("Gewicht: " + gewicht);
+
+
+        /** primitive Datentypen */
+        double meinGewicht = 79.2;
+
+        boolean istMaennlich = false; // true
+
+        short meinAlter = 34;
+
+
+        /** komplexe Datentypen */
+        String ausgabe = "Hallo Jan";
+
+        System.out.println(ausgabe);
+    }
+}
